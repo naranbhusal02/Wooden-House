6e5a59b85e4ccb90d4b6da0a901dd2d2a7d0e83d
नारायण भुसाल <110683302+naranbhusal02@users.noreply.github.com>
2025-04-06 09:26:30 +0545

    Add files via upload

---
+++ assets/js/carousel-data.js
@@ -0,0 +1,92 @@
+const carouselData = {
+  asianFoods: [
+    { 
+      image: '/test.png', 
+      title: 'Spicy Noodles', 
+      discount: '10%', 
+      stars: 4, 
+      price: { current: 49.00, original: 69.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Spring Rolls', 
+      discount: '69%', 
+      stars: 4, 
+      price: { current: 69.00, original: 69.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Tempura', 
+      discount: '5%', 
+      stars: 3, 
+      price: { current: 29.00, original: 39.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Fried Rice', 
+      discount: '20%', 
+      stars: 4, 
+      price: { current: 59.00, original: 79.00 } 
+    },
+  ],
+  japaneseFoods: [
+    { 
+      image: '/test.png', 
+      title: 'Sushi', 
+      discount: '10%', 
+      stars: 5, 
+      price: { current: 69.00, original: 89.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Ramen', 
+      discount: '15%', 
+      stars: 4, 
+      price: { current: 49.00, original: 59.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Tempura', 
+      discount: '5%', 
+      stars: 3, 
+      price: { current: 39.00, original: 49.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Tempura', 
+      discount: '20%', 
+      stars: 4, 
+      price: { current: 59.00, original: 79.00 } 
+    },
+  ],
+  drinks: [
+    { 
+      image: '/test.png', 
+      title: 'Mojito', 
+      discount: '10%', 
+      stars: 4, 
+      price: { current: 19.00, original: 29.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Bubble Tea', 
+      discount: '15%', 
+      stars: 5, 
+      price: { current: 14.00, original: 19.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Fruit Smoothie', 
+      discount: '5%', 
+      stars: 3, 
+      price: { current: 9.00, original: 12.00 } 
+    },
+    { 
+      image: '/test.png', 
+      title: 'Tempura', 
+      discount: '20%', 
+      stars: 4, 
+      price: { current: 59.00, original: 79.00 } 
+    },
+  ],
+};
