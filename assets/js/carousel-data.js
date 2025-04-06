9b48ad1d140368faa2ee02f3a9e9e673b13b30a0
नारायण भुसाल <110683302+naranbhusal02@users.noreply.github.com>
2025-04-06 09:34:34 +0545

    Update carousel-data.js

--- assets/js/carousel-data.js
+++ assets/js/carousel-data.js
@@ -1,28 +1,28 @@
 const carouselData = {
   asianFoods: [
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Spicy Noodles', 
       discount: '10%', 
       stars: 4, 
       price: { current: 49.00, original: 69.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Spring Rolls', 
       discount: '69%', 
       stars: 4, 
       price: { current: 69.00, original: 69.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Tempura', 
       discount: '5%', 
       stars: 3, 
       price: { current: 29.00, original: 39.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Fried Rice', 
       discount: '20%', 
       stars: 4, 
@@ -31,28 +31,28 @@ const carouselData = {
   ],
   japaneseFoods: [
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Sushi', 
       discount: '10%', 
       stars: 5, 
       price: { current: 69.00, original: 89.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Ramen', 
       discount: '15%', 
       stars: 4, 
       price: { current: 49.00, original: 59.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Tempura', 
       discount: '5%', 
       stars: 3, 
       price: { current: 39.00, original: 49.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Tempura', 
       discount: '20%', 
       stars: 4, 
@@ -61,28 +61,28 @@ const carouselData = {
   ],
   drinks: [
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Mojito', 
       discount: '10%', 
       stars: 4, 
       price: { current: 19.00, original: 29.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Bubble Tea', 
       discount: '15%', 
       stars: 5, 
       price: { current: 14.00, original: 19.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Fruit Smoothie', 
       discount: '5%', 
       stars: 3, 
       price: { current: 9.00, original: 12.00 } 
     },
     { 
-      image: '/test.png', 
+      image: 'https://iili.io/37Wd16X.png', 
       title: 'Tempura', 
       discount: '20%', 
       stars: 4, 
